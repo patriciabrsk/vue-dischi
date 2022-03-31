0cf7580af9aa15ba556c7443ec3fd4d2f99a4c3b
patriciabrsk <patriciabresku@gmail.com>
2022-03-31 12:40:05 +0200

    loading

--- src/components/RecordsList.vue
+++ src/components/RecordsList.vue
@@ -1,36 +1,41 @@
 <template>
-  <div class="container d-flex justify-content-center text-center pb-5">
-    <!-- <Loading v-if="recordsList <= 0" /> -->
-    <div class="row row-cols-5 g-4">
-      <RecordCard
-        v-for="(record, index) in recordsList"
-        :key="index"
-        :record="record"
-      />
+  <div
+    class="records-container d-flex flex-column align-items-center justify-content-center"
+  >
+    <Loading v-if="recordsList <= 0" />
+    <div class="container d-flex justify-content-center text-center h-100 pb-5">
+      <div class="row row-cols-5 g-4 h-100">
+        <RecordCard
+          v-for="(record, index) in recordsList"
+          :key="index"
+          :record="record"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import RecordCard from "./RecordCard.vue";
+import Loading from "./Loading.vue";
 import axios from "axios";
 
 export default {
   name: "RecordsList",
-  components: { RecordCard },
+  components: { RecordCard, Loading },
   data() {
     return {
       recordsList: [],
     };
   },
   created() {
-    // setTimeout(() => {
-    axios
-      .get("https://flynn.boolean.careers/exercises/api/array/music")
-      .then((response) => {
-        this.recordsList = response.data.response;
-      });
-    // }, 4000);
+    setTimeout(() => {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then((response) => {
+          this.recordsList = response.data.response;
+        });
+    }, 4000);
   },
 };
 </script>
